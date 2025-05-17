62f7893001c6c29a5d94e3691e00e91b42585778
Osaid-Mtawi <s12341143.stu.najah.edu>
2025-05-17 13:52:01 +0300

    s

--- Evento/routes/web.php
+++ Evento/routes/web.php
@@ -2,9 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RoleRequestController;
 
-Route::resource('rolerequest', RoleRequestController::class);
 Route::get('/', function () {
     return view('welcome');
 });
