a2b0d463a8e73b2bb082267ddf69c230638de16f
najihashayka <s12324171@stu.najah.edu>
2025-05-17 18:57:53 +0300

    donee

--- Evento/app/Http/Controllers/TicketsController.php
+++ Evento/app/Http/Controllers/TicketsController.php
@@ -7,15 +7,14 @@ class TicketsController extends Controller{
 
     public function index(Request $request){
         $user=$request->user();
-        if($user->profile->role==='Admin'){
-                    $tickets=Tickets::all();
+       if ($user && $user->profile && $user->profile->role === 'Admin') {
+    $tickets = Tickets::all();
+    return view('tickets.index', compact('tickets'));
+}
 
-            return view('tickets.index', compact('tickets'));
-        }
+$tickets = Tickets::where('user_id', $user->id)->get();
+return view('tickets.index', compact('tickets'));
 
-            $tickets=Tickets::where('user_id',$user->id)->get();
-
-            return view('tickets.index', compact('tickets'));
 
     }
 
