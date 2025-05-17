0639042e9552726962fc06832040d3a318a9e443
Fathi_Heelo <s12323885@stu.najah.edu>
2025-05-17 15:43:36 +0300

    done

--- Evento/routes/api.php
+++ Evento/routes/api.php
@@ -3,6 +3,7 @@ use App\Http\Controllers\EventController;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,22 @@ Route::post('/events', [EventController::class, 'store']);
 Route::get('/events', [EventController::class, 'index']);
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::delete('/events/{id}', [EventController::class, 'destroy']);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('events', EventController::class);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
