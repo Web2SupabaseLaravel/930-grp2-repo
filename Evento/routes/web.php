5b62813b0f52e5473105d27f453f9e777b34bbc2
najihashayka <s12324171@stu.najah.edu>
2025-05-17 18:40:09 +0300

    done

--- Evento/routes/web.php
+++ Evento/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketsController;
 
+
 Route::get('/tickets', [TicketsController::class, 'index']);
 Route::get('/', function () {
     return view('welcome');
