4d441b082f2f71d2feaf83cf9eb914cb149d2504
Fathi_Heelo <s12323885@stu.najah.edu>
2025-05-22 15:29:41 +0300

    done

--- Evento/routes/web.php
+++ Evento/routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
@@ -16,7 +15,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
+
     // Event routes
     Route::resource('events', EventController::class);
 });
