1f4888f3d26af3cfef7f420f1e7ee053c74a4099
Osaid-Mtawi <s12341143.stu.najah.edu>
2025-06-10 10:33:34 +0300

    finish all thingsssss

--- Evento/resources/js/components/Navbar.jsx
+++ Evento/resources/js/components/Navbar.jsx
@@ -5,28 +5,25 @@ import logo from '../assets/Evento.png';
 
 const NavigationBar = ({ menuItems }) => {
   return (
-    <Navbar expand="lg" className="py-3 shadow-sm">
-      <Container>
+    <Navbar className="py-3 shadow-sm">
+      <Container className="d-flex justify-content-between align-items-center">
         <Navbar.Brand href="#home" className="d-flex align-items-center">
           <img src={logo} alt="Evento Logo" className="me-2" style={{ height: '40px' }} />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto align-items-center">
-            {menuItems.map((item, index) => (
-              <Nav.Link 
-                key={index} 
-                href={item.link}
-                className="mx-2 fw-medium"
-              >
-                {item.text}
-              </Nav.Link>
-            ))}
-            <Nav.Link href="#profile" className="ms-3">
-              <FaUserCircle className="profile-icon" />
+        <Nav className="d-flex align-items-center">
+          {menuItems.map((item, index) => (
+            <Nav.Link 
+              key={index} 
+              href={item.link}
+              className="mx-2 fw-medium"
+            >
+              {item.text}
             </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+          ))}
+          <Nav.Link href="#profile" className="ms-3">
+            <FaUserCircle className="profile-icon" />
+          </Nav.Link>
+        </Nav>
       </Container>
     </Navbar>
   );
